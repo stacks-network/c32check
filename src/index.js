707b374a58192a840d8d613175a8048bf7330ce7
Jude Nelson <judecn@gmail.com>
2018-06-26 18:58:01 -0400

    initial implementation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import {
+  z32encode,
+  z32decode
+} from './encoding'
+
+import {
+  z32checkEncode,
+  z32checkDecode
+} from './checksum'
+
+export { z32encode, z32decode, z32checkEncode, z32checkDecode }
