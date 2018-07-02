8c127ac7525cff6b0ff3f8f0ff3e84ad84e917b0
Jude Nelson <judecn@gmail.com>
2018-07-02 17:20:41 -0400

    expose conversion between base58check and c32check addresses

--- src/index.js
+++ src/index.js
@@ -14,8 +14,10 @@ import {
 import {
   c32address,
   c32addressDecode,
+  c32ToB58,
+  b58ToC32,
   versions
 } from './address'
 
 export { c32encode, c32decode, c32checkEncode, c32checkDecode, 
-  c32address, c32addressDecode, c32normalize, versions }
+  c32address, c32addressDecode, c32normalize, versions, c32ToB58, b58ToC32 }
