a7482124b7293003c516841b876ee9d7b1859441
Jude Nelson <judecn@gmail.com>
2018-07-02 17:26:10 -0400

    add tests for c32toB58 and b58ToC32

--- tests/unitTests/src/index.js
+++ tests/unitTests/src/index.js
@@ -6,7 +6,9 @@ import {
   c32checkEncode,
   c32checkDecode,
   c32address,
-  c32addressDecode
+  c32addressDecode,
+  c32ToB58,
+  b58ToC32
 } from '../../../lib/index.js'
 
 export function c32encodingTests() {
@@ -577,10 +579,217 @@ export function c32addressTests() {
   })
 }
 
+export function c32ToB58Test() {
+  const hexStrings = [
+    'a46ff88886c2ef9762d970b4d2c63678835bd39d',
+    '0000000000000000000000000000000000000000',
+    '0000000000000000000000000000000000000001',
+    '1000000000000000000000000000000000000001',
+    '1000000000000000000000000000000000000000'
+  ]
+
+  const versions = [
+    22,
+    0,
+    31,
+    20,
+    26,
+    21
+  ]
+  
+  const c32addresses = [
+    [
+      'SP2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKNRV9EJ7', 
+      'SP000000000000000000002Q6VF78',
+      'SP00000000000000000005JA84HQ',
+      'SP80000000000000000000000000000004R0CMNV', 
+      'SP800000000000000000000000000000033H8YKK'
+    ],
+    [
+      'S02J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKPVKG2CE', 
+      'S0000000000000000000002AA028H', 
+      'S000000000000000000006EKBDDS', 
+      'S080000000000000000000000000000007R1QC00', 
+      'S080000000000000000000000000000003ENTGCQ'
+    ],
+    [
+      'SZ2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQ9H6DPR', 
+      'SZ000000000000000000002ZE1VMN', 
+      'SZ00000000000000000005HZ3DVN', 
+      'SZ80000000000000000000000000000004XBV6MS', 
+      'SZ800000000000000000000000000000007VF5G0'
+    ],
+    [
+      'SM2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQVX8X0G', 
+      'SM0000000000000000000062QV6X', 
+      'SM00000000000000000005VR75B2', 
+      'SM80000000000000000000000000000004WBEWKC', 
+      'SM80000000000000000000000000000000JGSYGV'
+    ],
+    [
+      'ST2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQYAC0RQ', 
+      'ST000000000000000000002AMW42H', 
+      'ST000000000000000000042DB08Y', 
+      'ST80000000000000000000000000000006BYJ4R4', 
+      'ST80000000000000000000000000000002YBNPV3'
+    ],
+    [
+      'SN2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKP6D2ZK9', 
+      'SN000000000000000000003YDHWKJ', 
+      'SN00000000000000000005341MC8', 
+      'SN800000000000000000000000000000066KZWY0', 
+      'SN800000000000000000000000000000006H75AK'
+    ]
+  ]
+
+  const b58addresses = [
+    [
+      'A7RjcihhakxJfAqgwTVsLTyc8kbhDJPMVY',
+      '9rSGfPZLcyCGzY4uYEL1fkzJr6fkicS2rs',
+      '9rSGfPZLcyCGzY4uYEL1fkzJr6fkoGa2eS',
+      '9stsUTaRHnyTRFWnbwiyCWwfpkkKCFYBD4',
+      '9stsUTaRHnyTRFWnbwiyCWwfpkkK9ZxEPC'
+    ],
+    [
+      '1FzTxL9Mxnm2fdmnQEArfhzJHevwbvcH6d',
+      '1111111111111111111114oLvT2',
+      '11111111111111111111BZbvjr',
+      '12Tbp525fpnBRiSt4iPxXkxMyf5Ze1UeZu',
+      '12Tbp525fpnBRiSt4iPxXkxMyf5ZWzA5TC'
+    ],
+    [
+      'DjUAUhPHyP8C256UAEVjhbRgoHvBetzPRR',
+      'DUUhXNEw1bNAMSKgm1Kt2tSPWdzF8952Np',
+      'DUUhXNEw1bNAMSKgm1Kt2tSPWdzFCMncsE',
+      'DVwJLSG1gR9Ln9mZpiiqZePkVJ4obdg7UC',
+      'DVwJLSG1gR9Ln9mZpiiqZePkVJ4oTzMnyD'
+    ],
+    [
+      '9JkXeW78AQ2Z2JZWtcqENDS2sk5orG4ggw',
+      '93m4hAxmCcGXMfnjVPfNhWSjb69sDziGSY',
+      '93m4hAxmCcGXMfnjVPfNhWSjb69sPHPDTX',
+      '95DfWEyqsS3hnPEcZ74LEGQ6ZkERn1FuUo',
+      '95DfWEyqsS3hnPEcZ74LEGQ6ZkERexa3xe'
+    ],
+    [
+      'Bin9Z9trRUoovuQ338q9Gy4kemdU7ni2FG',
+      'BTngbpkVTh3nGGdFdufHcG5TN7hXYuX31z',
+      'BTngbpkVTh3nGGdFdufHcG5TN7hXbks9tq',
+      'BVFHQtma8Wpxgz58hd4F922pLmn65qtPy5',
+      'BVFHQtma8Wpxgz58hd4F922pLmn5zEwasC'
+    ],
+    [
+      '9i68dcQQsaVRqjhbv3AYrLhpWFLkWkzrCG',
+      '9T6fgHG3unjQB6vpWozhBdiXDbQp3P7F8M',
+      '9T6fgHG3unjQB6vpWozhBdiXDbQp5FwEH5',
+      '9UZGVMH8acWabpNhaXPeiPftCFVNXQAYoZ',
+      '9UZGVMH8acWabpNhaXPeiPftCFVNMacQDQ'
+    ]
+  ]
+
+  const equivalentVersions = [
+    22,
+    20,
+    26,
+    21
+  ]
+   
+
+  const c32addressesEquivalentVersion = [
+    [
+      'SP2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKNRV9EJ7', 
+      'SP000000000000000000002Q6VF78',
+      'SP00000000000000000005JA84HQ',
+      'SP80000000000000000000000000000004R0CMNV', 
+      'SP800000000000000000000000000000033H8YKK'
+    ],
+    [
+      'SM2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQVX8X0G', 
+      'SM0000000000000000000062QV6X', 
+      'SM00000000000000000005VR75B2', 
+      'SM80000000000000000000000000000004WBEWKC', 
+      'SM80000000000000000000000000000000JGSYGV'
+    ],
+    [
+      'ST2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQYAC0RQ', 
+      'ST000000000000000000002AMW42H', 
+      'ST000000000000000000042DB08Y', 
+      'ST80000000000000000000000000000006BYJ4R4', 
+      'ST80000000000000000000000000000002YBNPV3'
+    ],
+    [
+      'SN2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKP6D2ZK9', 
+      'SN000000000000000000003YDHWKJ', 
+      'SN00000000000000000005341MC8', 
+      'SN800000000000000000000000000000066KZWY0', 
+      'SN800000000000000000000000000000006H75AK'
+    ]
+  ]
+
+  const b58addressesEquivalentVersion = [
+		[
+			'1FzTxL9Mxnm2fdmnQEArfhzJHevwbvcH6d',
+			'1111111111111111111114oLvT2',
+			'11111111111111111111BZbvjr',
+			'12Tbp525fpnBRiSt4iPxXkxMyf5Ze1UeZu',
+			'12Tbp525fpnBRiSt4iPxXkxMyf5ZWzA5TC'
+		],
+		[
+			'3GgUssdoWh5QkoUDXKqT6LMESBDf8aqp2y',
+			'31h1vYVSYuKP6AhS86fbRdMw9XHieotbST',
+			'31h1vYVSYuKP6AhS86fbRdMw9XHiiQ93Mb',
+			'339cjcWXDj6ZWt9KBp4YxPKJ8BNH7gn2Nw',
+			'339cjcWXDj6ZWt9KBp4YxPKJ8BNH14Nnx4'
+		],
+		[
+			'mvWRFPELmpCHSkFQ7o9EVdCd9eXeUTa9T8',
+			'mfWxJ45yp2SFn7UciZyNpvDKrzbhyfKrY8',
+			'mfWxJ45yp2SFn7UciZyNpvDKrzbi36LaVX',
+			'mgyZ7874UrDSCpvVnHNLMgAgqegGZBks3w',
+			'mgyZ7874UrDSCpvVnHNLMgAgqegGQUXx9c'
+		],
+		[
+			'2N8EgwcZq89akxb6mCTTKiHLVeXRpxjuy98',
+			'2MsFDzHRUAMpjHxKyoEHU3aMCMsVtMqs1PV',
+			'2MsFDzHRUAMpjHxKyoEHU3aMCMsVtXMsfu8',
+			'2MthpoMSYqBbuifmrrwgRaLJZLXaSyK2Rai',
+			'2MthpoMSYqBbuifmrrwgRaLJZLXaSoxBM5T'
+		]
+	]
+
+  test('c32ToB58 and b58ToC32', t => {
+    t.plan(hexStrings.length * versions.length * 2)
+    for (let i = 0; i < hexStrings.length; i++) {
+      for (let j = 0; j < versions.length; j++) {
+        const b58 = c32ToB58(c32addresses[j][i], versions[j])
+        const c32 = b58ToC32(b58addresses[j][i], versions[j])
+        t.equal(b58, b58addresses[j][i], `c32ToB58: expect ${b58addresses[j][i]}, got ${b58}`)
+        t.equal(c32, c32addresses[j][i], `b58ToC32: expect ${c32addresses[j][i]}, got ${c32}`)
+      }
+    }
+  })
+
+  test('c32ToB58 and b58ToC32 equivalent versions', t => {
+    t.plan(hexStrings.length * equivalentVersions.length * 2)
+    for (let i = 0; i < hexStrings.length; i++) {
+      for (let j = 0; j < equivalentVersions.length; j++) {
+        const b58 = c32ToB58(c32addressesEquivalentVersion[j][i])
+        const c32 = b58ToC32(b58addressesEquivalentVersion[j][i])
+        t.equal(b58, b58addressesEquivalentVersion[j][i], 
+          `c32ToB58: expect ${b58addressesEquivalentVersion[j][i]}, got ${b58}`)
+        t.equal(c32, c32addressesEquivalentVersion[j][i], 
+          `b58ToC32: expect ${c32addressesEquivalentVersion[j][i]}, got ${c32}`)
+      }
+    }
+  })
+}
+
+
 if (process.env.BIG_DATA_TESTS) {
   c32encodingRandomBytes()
 } else {
   c32encodingTests()
   c32checkEncodingTests()
   c32addressTests()
+  c32ToB58Test()
 }
